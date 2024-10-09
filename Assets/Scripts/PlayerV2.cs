3fa44e2ebe8bcdcd74e0d59e0c45aba22566a0c4
TomyagusDaVinci <tomas.gomez@davinci.edu.ar>
2024-10-09 19:54:16 -0300

    Barra de vida
    Agregue la barra de vida a la Interfaz

--- Assets/Scripts/PlayerV2.cs
+++ Assets/Scripts/PlayerV2.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerFPS : MonoBehaviour
 {
 
    public float _life;
    [SerializeField] private float _maxLife;
-    
+   [SerializeField] private Image BarraVida;
+
+    private void Update()
+    {
+        BarraVida.fillAmount = _life / 100;
+    }
+
     public void TakeDamage()
     {
 
