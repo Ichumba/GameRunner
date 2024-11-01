9a9704a2f7c6e80cc7e2f488923063284b5a52c6
Ichumba <ignacio.chumba@davinci.edu.ar>
2024-11-01 01:44:27 -0300

    Ordenamiento de scripts

--- Assets/Scripts/ScriptsPlayer/MovePj.cs
+++ Assets/Scripts/ScriptsPlayer/MovePj.cs
@@ -4,54 +4,15 @@ using UnityEngine;
 
 public class MovePj : MonoBehaviour
 {
-
-    private CharacterController controller;
-    public float speed = 5;
-    public float gravity = -9.81f * 2;
-    public float jumpHeight = 3f;
-    public Transform ground;
-    public float groundDis = 0.4f;
-    public LayerMask groundMask;
-
-    Vector3 velocity;
-    bool isGround;
-    bool isMov;
-
-    private Vector3 lastPos = new Vector3(0, 0, 0);   
+    // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        isGround = Physics.CheckSphere(ground.position, groundDis, groundMask);
-
-        if (isGround && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 Mov = transform.right * x + transform.forward * y;
-        controller.Move(speed * Time.deltaTime * Mov);
-
-        if (Input.GetButtonDown("Jump") && isGround)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-        if (lastPos != gameObject.transform.position && isGround == true)
-        {
-           isMov = true;
-        }
-        else
-        {
-            isMov = false;
-        }
-        lastPos = gameObject.transform.position;
+        
     }
 }
---
+++ Assets/Scripts/ScriptsPlayer/PlayerHP.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour
+{
+    public float _life;
+    [SerializeField] private float _maxLife;
+    [SerializeField] private Image BarraVida;
+
+    private void Update()
+    {
+        BarraVida.fillAmount = _life / 100;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _life -= damage;
+        if (_life < 0)
+        {
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
