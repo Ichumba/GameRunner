bce7eed0575c05c11a99d21e32fd9f194ac33b21
Ichumba <ignacio.chumba@davinci.edu.ar>
2024-11-13 23:54:33 -0300

    asd

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip SFX;
+    public AudioSource AudioSource;
+}
