032b49f8bb06f88aa005e021acfb573c0b3882f1
TomyagusDaVinci <tomas.gomez@davinci.edu.ar>
2024-11-30 21:09:56 -0300

    A

--- Assets/Scripts/ScriptsEnemy/Chaser.cs
+++ Assets/Scripts/ScriptsEnemy/Chaser.cs
@@ -7,6 +7,7 @@ public class Chaser : Enemy
 {
     [SerializeField] private float speed;
     [SerializeField] private float distancia = 20;
+    private bool cerca = false;
     public Animator _animator;
 
 
@@ -23,23 +24,29 @@ public class Chaser : Enemy
         {
             Destroy(this);
         }
+
+
+        if (Vector3.Distance(transform.position, player.position) <= distancia)
+        {
+            cerca = true;
+        }
     }
 
     void FixedUpdate()
     {
 
-        if (Vector3.Distance(transform.position, player.position) <= distancia)
-        {
+        
 
+
+        if (cerca== true) 
+        {
             Vector3 direction = player.position - transform.position;
             float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(-angle + 90f, Vector3.up);
 
             Quaternion rotation = Quaternion.Euler(0f, rb.rotation.y, 0f);
             rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * (Vector3)(rotation * transform.forward));
-
         }
-
        
        
     }
