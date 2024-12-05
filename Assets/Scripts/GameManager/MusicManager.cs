cb9df2a99b968fe436673888037b215d1148ba71
TomyagusDaVinci <tomas.gomez@davinci.edu.ar>
2024-12-04 21:16:59 -0300

    A

--- Assets/Scripts/GameManager/MusicManager.cs
+++ Assets/Scripts/GameManager/MusicManager.cs
@@ -27,33 +27,21 @@ public class MusicManager : MonoBehaviour
 
     [Header("UI")]
     [SerializeField] private Slider _masterSlider;
-    [SerializeField] private float _inMasterVol = 0.5f;
     [SerializeField] private Slider _musicSlider;
-    [SerializeField] private float _inMusicVol = 0.5f;
     [SerializeField] private Slider _sfxSlider;
-    [SerializeField] private float _inSFXVol = 1f;
 
-    void Start()
-    {
-        _masterSlider.value = _inMasterVol;
-        SetMasterVol(_inMasterVol);
-        _musicSlider.value = _inMusicVol;
-        SetMusicVol(_inMusicVol);
-        _sfxSlider.value = _inSFXVol;
-        SetSFXVol(_inSFXVol);
 
-    }
-    public void SetMasterVol(float value)
+    public void SetMasterVol()
     {
-        _mixer.SetFloat("MasterVol", value);
+        _mixer.SetFloat("MasterVol",Mathf.Log10(_masterSlider.value) * 20 );
     }
-    public void SetMusicVol(float value)
+    public void SetMusicVol()
     {
-        _mixer.SetFloat("MusicVol" , value);
+        _mixer.SetFloat("MusicVol" ,Mathf.Log10( _masterSlider.value)*20);
     }
-    public void SetSFXVol(float value)
+    public void SetSFXVol()
     {
-        _mixer.SetFloat("SFXVol", value);
+        _mixer.SetFloat("SFXVol", Mathf.Log10(_masterSlider.value) * 20);
     }
     public void PlayAudio(AudioClip clip)
     {
