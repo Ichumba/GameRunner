0bb6931b840a47752dd61e93da645d399990c728
TomyagusDaVinci <tomas.gomez@davinci.edu.ar>
2025-01-24 01:25:37 -0300

    A

--- Assets/Scripts/ScriptsEnemy/Turret.cs
+++ Assets/Scripts/ScriptsEnemy/Turret.cs
@@ -29,7 +29,7 @@ public class Turret : Enemy
     {
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(-angle+90, Vector3.up);
+        transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
     }
 
     IEnumerator Disparo(float Shot)
@@ -37,7 +37,7 @@ public class Turret : Enemy
        
         disparado = true;
         yield return new WaitForSeconds(Shot);
-        Bullet Proyectile = Instantiate(bala, ShootingPoint.position, transform.rotation* Quaternion.Euler(90f, 0f, 0f));
+        Bullet Proyectile = Instantiate(bala, ShootingPoint.position, transform.rotation* Quaternion.Euler(90f, 0f, -90f));
         Proyectile.damage = Damage;
         disparado = false;
     }
